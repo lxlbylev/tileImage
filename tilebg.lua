7c8aa22095b806a993a165f4c36d62f135abcf52
lxlbylev <104057012+lxlbylev@users.noreply.github.com>
2022-07-29 13:09:48 +0900

    Update tilebg.lua

--- tilebg.lua
+++ tilebg.lua
@@ -1,121 +1,92 @@
-------------------------------------------------------------------------        
---	==================
---	 tilebg.lua v1.0
---	==================
---
---  This creates a function tileBG() which will tile an image on the 
---  background of the display. It detects your display content height 
---  and attempts to calculate the minimum number of tiles needed (based 
---  on the size of the provided background tile.)
-------------------------------------------------------------------------  
---	---------
---	USAGE
---	---------
---
---	1) Include the function in your code (probably main.lua)
---
--- 	    require( "tilebg" )
---
---
---  2) Now you can begin to use the function in your code. 
---
---		local bg = tileBG() --add a new background, use the default settings
---		
---  The function takes 3 optional arguments:
---
---		bgImageFile - the filename of the background image to tile
---		bgWidth - the width of the bgImagefile
---		bgHeight - the height of the bgImagefile
--- 
---	Default values
---  If the function is run without any arguments then it will use these values:
---
---		bgImageFile = tileBG.jpg
---		bgWidth = 100
---		bgHeight = 100
---
---	In other words, you can quickly use this code if you have an image file
---  called tileBG.jpg in your project folder, which is 100x100 pixels in size.
---
---  Its best to store the background in a local object, as demoed above:
--- 		local bg = tileBG()
---
---	Doing this, you can then use an build in method to self-destruct the tiles:
---		bg.removeBG()
---
---
---  Thats all there is to it. There is a full demo on the github repo. To make
---  things easier, the function logs everything it does to the console...
---
-------------------------------------------------------------------------  
---	---------
---	CHANGELOG
---	---------
---
---	1.0 - First release
---
-------------------------------------------------------------------------        
 
-function tileBG(bgImageFile,bgWidth,bgHeight)
+local function tileImage(ImagePath,width,height,tileSize,offSetX,offSetY)
    
-   print ("TileBG started.")
-   
-	--Set the defaults, for rapid calls and just in case
-	bgImageFile = bgImageFile or "tileBG.jpg"
-	bgWidth = bgWidth or 100
-	bgHeight = bgHeight or 100
+	local isImageExsist = display.newImageRect(ImagePath,1,1)
+	if not isImageExsist then error("TileImage: bad argument #1 (image '".. ImagePath .. "'' not found)") end
+	if type(width)~="number" then error("TileImage: bad argument #2 (number expected, got ".. type(width) ..")") end
+	if type(height)~="number" then error("TileImage: bad argument #3 (number expected, got ".. type(height) ..")") end
+	if type(tileSize)~="number" then error("TileImage: bad argument #4 (number expected, got ".. type(tileSize) ..")") end
+	display.remove(isImageExsist)
 	
 	--Calculate how manytimes to repeat the image based on display size, math.ceil roundup for loops
-	local bgRepeatX=math.ceil(display.contentWidth/bgWidth) --number of times to repeat bg on x axis
-	local bgRepeatY=math.ceil(display.contentHeight/bgHeight) --number of times to repeat bg on y axis
-	local bgCount = bgRepeatX*bgRepeatY --total bg images placed
-   
-	--Useful debug info
-	print ("TileBG: display.contentWidth: " .. display.contentWidth)
-	print ("TileBG: display.contentHeight: " .. display.contentHeight)
-	print ("TileBG: bgImageFile: " .. bgImageFile)
-	print ("TileBG: bgWidth: " .. bgWidth)
-	print ("TileBG: bgHeight: " .. bgHeight)
-	print ("TileBG: bgRepeatX: " .. bgRepeatX .. "(number of times to repeat bg on X axis)")
-	print ("TileBG: bgRepeatY: " .. bgRepeatY .. "(number of times to repeat bg on Y axis)")
-	
-	--Loop through and place the bg image
-	local bgImages = {} --images table
-	local bgLocationY = 0 --start at origin
-	local bgLocationX = 0 --start at origin
-	
-	local tiledBG=display.newGroup()
+	local bgRepeatX=math.ceil(width/tileSize) --number of times to repeat bg on x axis
+	local bgRepeatY=math.ceil(height/tileSize) --number of times to repeat bg on y axis
 
-	for bgY=1,bgRepeatY do
-		local bgLocationX = 0 --reset x position each time we go to next row
-		for bgX=1,bgRepeatX do
-			local img = display.newImage( bgImageFile, bgLocationX, bgLocationY );
-			img.anchorX, img.anchorY = 0, 0
-			tiledBG:insert(img)
-			print ("TileBG: Placed tile at x,y: " .. bgLocationX .. "," .. bgLocationY)
-			bgLocationX=bgLocationX+bgWidth
-		end
-		bgLocationY=bgLocationY+bgHeight
+	if offSetX~=nil and offSetX~=0 then
+		local ost = math.abs(offSetX)%tileSize
+		if offSetX>0 then offSetX = ost
+		else offSetX = -ost end
+	else
+		offSetX = 0
 	end
 
-	tiledBG:toBack() --make sure its on the background
+	if offSetY~=nil and offSetY~=0 then
+		local ost = math.abs(offSetY)%tileSize
+		if offSetY>0 then offSetY = ost
+		else offSetY = -ost end
+	else
+		offSetY = 0
+	end
+
+	--Useful debug info
+	-- print ("TileImage: ImagePath: " .. ImagePath)
+	-- print ("TileImage: width: " .. width)
+	-- print ("TileImage: height: " .. height)
+	-- print ("TileImage: bgRepeatX: " .. bgRepeatX .. " (number of times to repeat bg on X axis)")
+	-- print ("TileImage: bgRepeatY: " .. bgRepeatY .. " (number of times to repeat bg on Y axis)")
+	-- print ("TileImage: offSetX: " .. offSetX)
+	-- print ("TileImage: offSetY: " .. offSetY)
 	
-   print ("TileBG: Placed " .. bgCount .. " images on background.")
-   
+	--Loop through and place the image
+	local tilesGroup = display.newGroup()
+
+	local container = display.newContainer(width,height)
+	tilesGroup:insert(container)
+	container:translate(width*.5, height*.5)
+
+	local startX = -width*.5 --start at origin
+	if offSetX~=0 then
+		bgRepeatX = bgRepeatX + 1
+		startX = startX + offSetX - tileSize * (offSetX > 0 and 1 or 0)
+	end
+
+	local bgLocationY = -height*.5 --start at origin
+	if offSetY~=0 then
+		bgRepeatY = bgRepeatY + 1
+		bgLocationY = bgLocationY + offSetY - tileSize * (offSetY > 0 and 1 or 0)
+	end
+
+	for bgY=1,bgRepeatY do
+		local bgLocationX = startX --reset x position each time we go to next row
+		for bgX=1,bgRepeatX do
+			local tile = display.newImageRect( ImagePath, tileSize, tileSize )
+			container:insert( tile)
+			tile.x, tile.y = bgLocationX, bgLocationY
+			tile.anchorX, tile.anchorY = 0, 0
+			-- print ("TileImage: Placed tile at x,y: " .. bgLocationX .. "," .. bgLocationY)
+			bgLocationX=bgLocationX+tileSize
+		end
+		bgLocationY=bgLocationY+tileSize
+	end
+
+    -- print ( "textureMemoryUsed: " .. system.getInfo( "textureMemoryUsed" ) )
+   	
    	--a function to cleanup the bg
-	function tiledBG.removeBG ()
-		if tiledBG.numChildren then
-			 print ("TileBG: Removing " .. tiledBG.numChildren .. " images from background.")
-			while tiledBG.numChildren > 0 do   -- we have have tiles left in the tiledbg group, so first clean them out		
-					tiledBG:remove(tiledBG.numChildren) -- clean out the last member of the group (work from the top down!)
+	function tilesGroup.remove()
+		if container.numChildren then
+			-- print ("TileImage: Removing " .. container.numChildren .. " images from background.")
+			while container.numChildren > 0 do   -- we have have tiles left in the tilesGroup group, so first clean them out		
+				container:remove(container.numChildren) -- clean out the last member of the group (work from the top down!)
 			end
 		end	
-		tiledBG:removeSelf() --whats left is now an empty group. remove it.
-	    print ("TileBG: Removed background.")
+		container:removeSelf() --whats left is now an empty group. remove it.
+		tilesGroup:removeSelf() --whats left is now an empty group. remove it.
+	    -- print ("TileImage: Removed")
 		return
 	end
  
    --Send everything back to mama
-   return tiledBG
-  
- end
+   return tilesGroup
+end
+
+return tileImage
